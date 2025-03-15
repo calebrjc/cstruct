b568a4cdb5e4800ad368e55b22c71fceeab01baa
Caleb Johnson-Cantrell <46861012+calebrjc@users.noreply.github.com>
2025-03-15 15:16:44 -0400

    feat: Simplify the implementation of `cstruct_sizeof`

--- src/cstruct.c
+++ src/cstruct.c
@@ -8,20 +8,28 @@
  */
 
 /// Return true if the character is a digit, and false otherwise.
-/// @param [in] c Character to check.
+/// @param[in] c Character to check.
 /// @return True if the character is a digit, and false otherwise.
 static inline bool __cstruct_isdigit(char c);
 
+/// @brief Return the next multiplier for the given format string, and advance the index to that of
+///        the next format character.
+/// @param format The format string.
+/// @param[inout] i On entry, the supposed start index of the multiplier.
+///                 On exit, the index of the supposed next format character.
+/// @return The multiplier, or -1 if the format string is invalid.
+static int32_t __cstruct_parse_multiplier(const char *format, size_t *i);
+
 /// Return the size of the type which the given character represents.
-/// @param [in] c Character to check.
-/// @param [in] multiplier Multiplier to apply to the size.
+/// @param[in] c Character to check.
+/// @param[in] multiplier Multiplier to apply to the size.
 /// @return The size of the type which the given character represents multiplied by the given
 ///         multiplier, or 0 if the character is not a valid type.
 static size_t __cstruct_calculate_size(char c, int multiplier);
 
 size_t cstruct_sizeof(const char *format)
 {
-    if (!format)
+    if (!format || *format == '\0')
     {
         return 0;
     }
@@ -29,29 +37,13 @@ size_t cstruct_sizeof(const char *format)
     size_t i = 0, total_size = 0;
     while (format[i] != '\0')
     {
-        bool    multiplier_set = false;
-        int32_t multiplier     = 1;
-
-        while (__cstruct_isdigit(format[i]))
-        {
-            if (!multiplier_set)
-            {
-                multiplier_set = true;
-                multiplier     = 0;
-            }
-
-            multiplier *= 10;
-            multiplier += format[i] - '0';
-            i++;
-        }
-
-        // NOTE(Caleb): If the multiplier somehow gets overflowed, just return 0. I don't intend to
-        //              support structures with sizes so large that an int32_t can't hold it.
-        if (multiplier < 0)
+        int32_t multiplier = __cstruct_parse_multiplier(format, &i);
+        if (multiplier <= 0)
         {
             return 0;
         }
 
+        // NOTE(Caleb): At this point, format[i] is the next format character.
         size_t size = __cstruct_calculate_size(format[i], multiplier);
         if (size == 0)
         {
@@ -70,11 +62,40 @@ static inline bool __cstruct_isdigit(char c)
     return '0' <= c && c <= '9';
 }
 
+static int32_t __cstruct_parse_multiplier(const char *format, size_t *i)
+{
+    // NOTE(Caleb):
+    // - Assume that format is not NULL and is not empty, as the calling function should have
+    //   already validated this.
+    // - Assume that i is not NULL, as the calling function should have already validated this.
+
+    if (!__cstruct_isdigit(format[*i]))
+    {
+        return 1;
+    }
+
+    int32_t multiplier = 0;
+    do
+    {
+        multiplier = (multiplier * 10) + (format[*i] - '0');
+        (*i)++;
+    } while (__cstruct_isdigit(format[*i]));
+
+    // NOTE(Caleb):
+    // If the multiplier somehow gets overflowed, just return 0 (invalid format).
+    // I don't intend to support structures with sizes larger than a positive int32_t.
+    if (multiplier <= 0)
+    {
+        return -1;
+    }
+
+    return multiplier;
+}
+
 static size_t __cstruct_calculate_size(char c, int multiplier)
 {
     size_t base_size = 0;
 
-    // TODO(Caleb): Implement 'p'
     switch (c)
     {
         case 'x':
--- src/cstruct.h
+++ src/cstruct.h
@@ -3,37 +3,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/*
- * Structs can be specified with a format string similar to those specified in Python's struct
- * module.
- *
- * NOTE: Until a later version, structs and their sizes will be implemented using network byte order
- *       (see: "!" in the Python struct module), though they will be packed from and unpacked into
- *       the native byte order.
- *
- * | Specifier |     Type      | Size |
- * |-----------|---------------|------|
- * |     x     |   nul byte    |  1   |
- * |     b     |   int8_t      |  1   |
- * |     B     |   uint8_t     |  1   |
- * |     h     |   int16_t     |  2   |
- * |     H     |   uint16_t    |  2   |
- * |     i     |   int32_t     |  4   |
- * |     I     |   uint32_t    |  4   |
- * |     l     |   int32_t     |  4   |
- * |     L     |   uint32_t    |  4   |
- * |     q     |   int64_t     |  8   |
- * |     Q     |   uint64_t    |  8   |
- * |     f     |   float       |  4   |
- * |     d     |   double      |  8   |
- * |     s     |   string      |  _   |
- * |     p     | pascal string |  _   | (not implemented yet)
- *
- * NOTE: A multiplier can be appended to a size specifier to indicate repeating that specifier
- *       multiple times.
- */
-
 /// Return the size of a packed struct given its format string.
-/// @param [in] format The format string.
+/// @param[in] format The format string.
 /// @return The size of the packed struct, or zero if the format string is invalid.
 size_t cstruct_sizeof(const char *format);
